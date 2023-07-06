9e2fde0ca231aab6a58fbb48b931d91c3148c304
tomcii <116713@fhwn.ac.at>
2023-07-06 23:24:00 +0200

    Change design

--- src/app/components/dropdown-menu/dropdown-menu.component.ts
+++ src/app/components/dropdown-menu/dropdown-menu.component.ts
@@ -1,6 +1,5 @@
 import {Component, Input} from '@angular/core';
 import {Router} from '@angular/router';
-import {ItemService} from "../../service/item-data/item.service";
 import {HttpClient} from "@angular/common/http";
 /**
  * @title Basic menu
